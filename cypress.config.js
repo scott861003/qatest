ed2cdee26594ba32cdc226ca745982715061398d
Scott Fan <scott.fan@linexsolutions.com>
2025-08-14 13:37:52 +0800

    modify yaml

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
+const { defineConfig } = require('cypress')
 module.exports = {
+  
   e2e: {
+    baseUrl: process.env.CYPRESS_baseUrl || 'https://example.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
