d93d8d35589774c495147011ac64acf5e7a6601b
Scott Fan <scott.fan@linexsolutions.com>
2025-08-28 15:35:58 +0800

    reporter add

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 
 module.exports = {
-  
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: "[datetime]-test-report",
+    timestamp: "isoDate",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 };
