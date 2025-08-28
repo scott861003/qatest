f1a2ba8c4f4c35e1e78495651bff5ed6bf1110f3
Scott Fan <scott.fan@linexsolutions.com>
2025-08-28 15:56:52 +0800

    reporter add

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      // 123
     },
   },
 };
